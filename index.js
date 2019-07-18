684913623329982371fe3d22f74d94b20a125f5f
Samuel Phineas Hecht <hechtsp@gmail.com>
2019-07-18 16:13:12 -0700

    vole loom

--- index.js
+++ index.js
@@ -7,12 +7,17 @@
 
 var oneAudio = document.getElementById("one-audio");
 var oneSeek = document.getElementById("one-seek");
-
+var oneVol = document.getElementById("one-vol")
 
 function onePlay(){oneAudio.play()}
 function oneStop(){oneAudio.pause(); oneAudio.currentTime=0;}
 
-function oneMaxDurSet(){ oneSeek.max = oneAudio.duration;}
+function oneMaxDurSet(){oneSeek.max = oneAudio.duration;}
 function oneDurSet(){oneSeek.value = oneAudio.currentTime};
 function oneSeekSet(){oneAudio.currentTime = oneSeek.value;}
-function oneReSet() { oneAudio.play(); oneSeek.value = oneAudio.currentTime;}
+function oneReSet() {oneAudio.play(); oneSeek.value = oneAudio.currentTime;}
+
+function oneVolumeSet() {oneAudio.volume = oneVol.value;}
+
+setInterval(function(){console.log(oneAudio.volume)}, 1000)
+
