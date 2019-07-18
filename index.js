d977c08abb68cb2553ace90003fb6c95032a9356
Samuel Phineas Hecht <hechtsp@gmail.com>
2019-07-18 16:02:47 -0700

    smart loop

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-
 // console.log(audio.paused);
 // audio.muted=true
 // setInterval(function(){console.log(audio.paused)}, 1000)
 // audio.loop=false
 // console.log(audio.seekable)
+// setTimeout(function(){oneAudio.play()}, 3000)
 
 var oneAudio = document.getElementById("one-audio");
 var oneSeek = document.getElementById("one-seek");
 
-setTimeout(function(){oneAudio.play()}, 3000)
-oneAudio.loop=true
 
-function oneMaxDurSet(){ oneSeek.max = oneAudio.duration}
-function oneDurSet(){oneSeek.value = oneAudio.currentTime}
-function oneSeekSet(){oneAudio.currentTime = oneSeek.value}
-// function oneDurReSet(){oneSeek.value = oneAudio.currentTime}
 function onePlay(){oneAudio.play()}
 function oneStop(){oneAudio.pause(); oneAudio.currentTime=0;}
+
+function oneMaxDurSet(){ oneSeek.max = oneAudio.duration;}
+function oneDurSet(){oneSeek.value = oneAudio.currentTime};
+function oneSeekSet(){oneAudio.currentTime = oneSeek.value;}
+function oneReSet() { oneAudio.play(); oneSeek.value = oneAudio.currentTime;}
