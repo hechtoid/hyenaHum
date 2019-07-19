5fb24b580f5b4dbd3078971a2ecfa9497d4bd00b
Samuel Phineas Hecht <hechtsp@gmail.com>
2019-07-19 16:47:08 -0700

    finish js oop and wish for html oop aka miss react

--- index.js
+++ index.js
@@ -21,22 +21,13 @@ this.soundPauseButton  = soundPauseButton
 
 //TESTING ONLY
 // audio.loop=false
-this.soundAudio.muted = true
+// this.soundAudio.muted = true
+this.soundAudio.volume = 0.25
 setInterval(function(){console.log(soundAudio)}, 1000)
+// setInterval(){(console.log(soundAudio.src), 1000)}
+// setInterval(function(){console.log(soundSelector.value)}, 1000)
 }
 
-soundPlay(){
-    this.soundAudio.play();
-    // if (soundAudio.src.includes('none')){
-    //     soundDiv.classList.remove("playing")
-    //     soundPauseButton.classList.add("inactive");
-    //     soundPlayButton.classList.remove("inactive");
-    // } else { 
-        this.soundDiv.classList.add("playing")
-        this.soundPlayButton.classList.add("inactive");
-        this.soundPauseButton.classList.remove("inactive");
-    // }
-}
 soundToggle(){
     if (this.soundAudio.paused && !this.soundAudio.src.includes('none')){
         this.soundAudio.play();
@@ -50,23 +41,27 @@ soundToggle(){
         this.soundPlayButton.classList.remove("inactive");
     }
 }
+soundPlay(){
+    this.soundAudio.play();
+    // if (soundAudio.src.includes('none')){
+    //     soundDiv.classList.remove("playing")
+    //     soundPauseButton.classList.add("inactive");
+    //     soundPlayButton.classList.remove("inactive");
+    // } else { 
+        this.soundDiv.classList.add("playing")
+        this.soundPlayButton.classList.add("inactive");
+        this.soundPauseButton.classList.remove("inactive");
+    // }
+}
 soundPause() { 
     this.soundAudio.pause(); 
     this.soundAudio.currentTime = 0; 
     this.soundDiv.classList.remove("playing")
 }
-// soundCheck() {
-//     soundAudio.paused
-// }
-
 soundVolumeSet(){
     this.soundAudio.muted = false;
     this.soundAudio.volume = this.soundVol.value;
 }
-
-// setInterval(){(console.log(soundAudio.src), 1000)}
-// setInterval(function(){console.log(soundSelector.value)}, 1000)
-
 alertUpload() { 
     let that = this
     alert("Coming Soon!!"); 
@@ -87,15 +82,18 @@ soundSelect(){
         this.soundPlay();
     }
 }
-    
-    // soundMaxDurSet(){soundSeek.max = soundAudio.duration;}
-    // soundDurSet(){soundSeek.value = soundAudio.currentTime};
-    // soundSeekSet(){soundAudio.currentTime = soundSeek.value; soundPlay()}
+//SEEKBAR 
+// soundMaxDurSet(){soundSeek.max = soundAudio.duration;}
+// soundDurSet(){soundSeek.value = soundAudio.currentTime};
+// soundSeekSet(){soundAudio.currentTime = soundSeek.value; soundPlay()}
 // soundReSet(){
 //     this.soundAudio.currentTime = 0;
 //     this.soundPlay()
 // }
+
 }
 
 const oneSound = new Sound('one')
 const twoSound = new Sound('two')
+const threeSound = new Sound('three')
+const fourSound = new Sound('four')
