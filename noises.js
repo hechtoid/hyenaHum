493652ec1f4e21ab50f98bfe3fd4908c348f8a7b
Samuel Phineas Hecht <hechtsp@gmail.com>
2019-08-20 11:44:17 -0700

    ico

--- noises.js
+++ noises.js
@@ -2,7 +2,8 @@
 let noiseSelector = document.getElementById("noise-selector");
 
 var AudioContext = window.AudioContext || window.webkitAudioContext;
-var context = new AudioContext
+
+
 // var gainNode = context.createGain();
 // var whiteNoise = context.createWhiteNoise();
 // 
@@ -13,22 +14,21 @@ var context = new AudioContext
 
 
 function noiseSelect() {
-    
-
+    var context = new AudioContext
+    var whiteNoise = context.createWhiteNoise();
+    var pinkNoise = context.createPinkNoise();
+    var brownNoise = context.createBrownNoise();
     switch (noiseSelector.value) {
         case 'none':
-            // whiteNoise.disconnect();
+            context.close()
             break;
         case 'white':
-            var whiteNoise = context.createWhiteNoise();
             whiteNoise.connect(context.destination);
             break;
         case 'pink':
-            var pinkNoise = context.createPinkNoise();
             pinkNoise.connect(context.destination);
             break;
         case 'brown':
-            var brownNoise = context.createBrownNoise();
             brownNoise.connect(context.destination);
             break;
         case 'custom':
